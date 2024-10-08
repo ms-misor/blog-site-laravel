051a27e76adb9da55cace1dc1f8b7b4112928664
ms-misor <msmisor5@gmail.com>
2024-10-08 08:27:18 +0600

    Techers page in website

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -24,20 +24,6 @@
 			</div>
 			<!-- Right SIdebar -->
 			<div class="col-md-4">
-				<!-- Search -->
-				<div class="card mb-4">
-					<h5 class="card-header">Search</h5>
-					<div class="card-body">
-						<form action="{{url('/')}}">
-							<div class="input-group">
-							  <input type="text" name="q" class="form-control" />
-							  <div class="input-group-append">
-							    <button class="btn btn-dark" type="button" id="button-addon2">Search</button>
-							  </div>
-							</div>
-						</form>
-					</div>
-				</div>
 				<!-- Recent Posts -->
 				<div class="card mb-4">
 					<h5 class="card-header">Recent Posts</h5>
--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -207,6 +207,9 @@
 		          <a class="dropdown-item" href="#">Mathematics Club</a>
 		        </div>
 		      </li>
+		      <li class="nav-item">
+		       <a class="nav-link" href="{{url('all-teachers')}}">Teachers</a>
+		      </li>
 
 		      <!-- li class="nav-item dropdown">
 		        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
